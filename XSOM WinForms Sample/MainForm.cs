b5826094c59e257bcd881675be8ff43b51879aa2
Denis Molodtsov <denisM@jolera.com>
2016-10-22 11:18:44 -0400

    Added a new image

--- XSOM WinForms Sample/MainForm.cs
+++ XSOM WinForms Sample/MainForm.cs
@@ -39,19 +39,30 @@ namespace FinFormsSample
             XUser user = await context.GetCurrentUser();
             XList list = await context.GetList("Tests");
 
-            //XListItem listItem = await list.GetItemById(23);
-            //string caml = "<Where><Eq><FieldRef Name='Title' /><Value Type='Text'>TestUpdated</Value></Eq></Where>";
-            string currentUserCaml = "<Where><Eq><FieldRef Name='Author' LookupId='True' /><Value Type='Integer'><UserID /></Value></Eq></Where>";
-            List<XListItem> listItems = await list.GetItems(currentUserCaml);
-            //List<XListItem> listItems = await list.GetItems();
-            //await listItems[0].Delete();
-            //await listItems[0].Recycle();
-            //XListItem item = listItems[0];
-            //item["Title"] = "new title";
-            //await item.Update();
+				//List<XListItem> listItems = await list.GetItems();
+				//XListItem listItem = await list.GetItemById(23);
 
-            //Upload a photo:
-            byte[] imageBytes = File.ReadAllBytes("photo.jpg");
+				string currentUserCaml = 
+					@"<Where>
+						<Eq>
+							<FieldRef Name='Author' LookupId='True' />
+							<Value>
+								Type='Integer'>
+								<UserID />
+							</Value>
+						</Eq>
+					</Where>";
+
+				List<XListItem> listItems = await list.GetItems(currentUserCaml);
+				
+            XListItem item = listItems[0];
+            item["Title"] = "new title";
+            await item.Update();
+				await listItems[0].Delete();
+				await listItems[0].Recycle();
+
+			//Upload a photo:
+			byte[] imageBytes = File.ReadAllBytes("photo.jpg");
             string destinationFileName = string.Format("Photo_{0}.jpg", Guid.NewGuid());
             string result = await context.UploadFile(imageBytes, "/Photos/", destinationFileName);
             var file = await context.GetFile("/Photos/", destinationFileName);
